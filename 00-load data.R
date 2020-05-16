13d7c89bdcdc66baf8c16ce445d7bb28b062c8b1
jjhall77 <jjh562@nyu.edu>
2020-05-16 10:47:56 -0400

    test

--- 00-load data.R
+++ 00-load data.R
@@ -2,3 +2,13 @@
 library(tidyverse)
 library(lubridate)
 library(janitor)
+library(sf)
+library(RSocrata)
+
+url = 'https://data.cityofnewyork.us/Public-Safety/NYPD-Shooting-Incident-Data-Historic-/833y-fsy8'
+url_ytd = 'https://data.cityofnewyork.us/Public-Safety/NYPD-Shooting-Incident-Data-Year-To-Date-/5ucz-vwe8/data'
+
+shootings_historical <-read.socrata(url,stringsAsFactors = FALSE)
+shootings_ytd <- read.socrata(url_ytd,stringsAsFactors = FALSE)
+
+shootings <- rbind(shootings_historical, shootings_ytd) #bind_rows doesn't coerce characters
